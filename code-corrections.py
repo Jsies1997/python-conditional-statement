87e63e18a10fe7ebdf24fa7d442d4a219da70d66
Jsies1997 <jsies1997@icloud.com>
2024-05-28 16:10:51 -0500

    saving  progress

---
+++ code-corrections.py
@@ -0,0 +1,23 @@
+#task 1
+
+number =int(input("Enter a number: "))
+
+if number > 0:
+    print("The number is positive.")
+elif number == 0:
+    print("The number is zero.")
+else:
+    print("The number is negative.")
+
+#task 1
+
+num1 = (int(input("input a number")))
+num2 = (int(input("enter a number")))
+num3 = (int(input("enter a number")))
+if num1 >= num2 and num1>= num3:
+     print(num1)
+elif num2 >= num1 and num2 >= num3:
+    print(num2)
+else:
+    print(num3)
+    
---
+++ the-greatest-showdown.py
@@ -0,0 +1,28 @@
+#task 1
+
+num1 = (int(input("input a number")))
+num2 = (int(input("enter a number")))
+num3 = (int(input("enter a number")))
+if num1 >= num2 and num1 >= num3:
+     print(num1)
+elif num2 >= num1 and num2 >= num3:
+    print(num2)
+else:
+    print(num3)
+    
+    #task 2
+    
+   
+    
+    if num1 <= num2 and num1 <= num3:
+        print(num1)
+    elif num2 <= num1 and num2 <= num3:
+        print(num2)
+    else:
+        print(num3)
+        
+#task 3
+
+if num1 == num2 and num1 == num3:
+    print("all the numbers are equal")
+
