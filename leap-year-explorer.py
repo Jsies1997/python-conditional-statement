4c881c9629ed1c7d70258eeec41389773c5fe973
Jsies1997 <jsies1997@icloud.com>
2024-05-28 19:15:04 -0500

    last part of assignment

---
+++ leap-year-explorer.py
@@ -0,0 +1,9 @@
+#task 1
+year = int(input("enter a year"))
+
+if year % 400 == 0 and year %100 == 0:
+    print("is a leap year")
+elif year % 4 == 0 and year % 100 != 0:
+    print("is a leap year")
+else:
+    print("is not a leap year")
