d7f3b369f9308a9732d4954ea7f86b50134e9e3d
Jsies1997 <jsies1997@icloud.com>
2024-05-28 19:05:44 -0500

    modified print statement

--- the-greatest-showdown.py
+++ the-greatest-showdown.py
@@ -4,10 +4,13 @@ num1 = (int(input("input a number")))
 num2 = (int(input("enter a number")))
 num3 = (int(input("enter a number")))
 if num1 >= num2 and num1 >= num3:
+     print("the greatest number is")
      print(num1)
 elif num2 >= num1 and num2 >= num3:
+    print("the greatest number is")
     print(num2)
 else:
+    print("the greatest number is")
     print(num3)
     
     #task 2
@@ -15,14 +18,17 @@ else:
    
     
     if num1 <= num2 and num1 <= num3:
+        print("the smallest number is")
         print(num1)
     elif num2 <= num1 and num2 <= num3:
+        print("the smallest number is")
         print(num2)
     else:
+        print ("the smallest number is")
         print(num3)
         
 #task 3
 
 if num1 == num2 and num1 == num3:
-    print("all the numbers are equal")
+    print("all numbers are equal")
 
